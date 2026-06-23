321512966dfc7c2a13d423d6346f0e2c66859fb3
Douglasdevif <douglasdevif@gmail.com>
2026-06-23 15:13:24 +0000

    Redimensionar

--- unidade03/2025_1/q2e3/header/TabelaHash.h
+++ unidade03/2025_1/q2e3/header/TabelaHash.h
@@ -91,12 +91,12 @@ private:
             Elemento& elemento = arrayVelho[i];
 
             if(elemento.estado == Estado::OCUPADO){
-                int h = this->hash(elemento.chave);
-                for(int delta = 0; delta < novaCapacidade; delta ++){
+                auto h = this->hash(elemento.chave);
+
+                for(int delta = 0; delta < novaCapacidade; delta++){
                     auto indice = (h + delta)%novaCapacidade;
 
                     if(arrayNovo[indice].estado == Estado::LIVRE){
-
                         arrayNovo[indice].chave = elemento.chave;
                         arrayNovo[indice].valor = elemento.valor;
                         arrayNovo[indice].estado = Estado::OCUPADO;
